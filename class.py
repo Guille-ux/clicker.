a453c301c0f331806ec0e59acb718a4f4760ebf1
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2023-01-14 18:36:56 +0000

    first part

---
+++ class.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+import json
+
+class hc():
+   def _init_(self, c):
+      self.c = c
+   def g(self):
+      g = TK()
+      g.
+      ttk.Button(g, command = click)
+      
